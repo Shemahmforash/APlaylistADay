6d37ad6c1ccf6e50b5569458ea18e1280d0fda24
Carlos Rosão <shemahmforash@gmail.com>
2013-08-14 21:42:00 +0100

    Getting artists from events implemented.

--- lib/APlaylistADay/Events.pm
+++ lib/APlaylistADay/Events.pm
@@ -15,8 +15,27 @@ sub get {
     my $json   = $ua->get( $self->app->{config}->{'events'}->{'url'} );
     my $events = $json->res->json;
 
-    #TODO: find artists from events
+    #for each event, try to find an artist using echonest
+    my $base = sprintf(
+        "%s?api_key=%s&format=json&results=1",
+        $self->app->{config}->{'echonest'}->{'extract'},
+        $self->app->{config}->{'echonest'}->{'key'}
+    );
+    my @artists;
+    for my $event ( @{ $events || [] } ) {
+        my $url = sprintf( '%s&text=%s', $base, $event->{'description'} );
+
+        $json = $ua->get($url);
+        my $result = $json->res->json;
+
+        #no error in call
+        if ( $result->{'response'}->{'status'}->{'code'} == 0 ) {
+            push @artists, pop $result->{'response'}->{'artists'};
+        }
+    }
+
     #TODO: generate playlist from the artists
+    #check: https://developers.google.com/youtube/v3/
 
     my $message = "Events";
     if ( defined $day && defined $month ) {
