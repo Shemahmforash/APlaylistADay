bc08560a1bd4c32f5272d39727ac91910a665e81
Carlos Rosão <shemahmforash@gmail.com>
2013-08-14 18:06:47 +0100

    Read events from external service

--- lib/APlaylistADay/Events.pm
+++ lib/APlaylistADay/Events.pm
@@ -1,10 +1,22 @@
 package APlaylistADay::Events;
+
 use Mojo::Base 'Mojolicious::Controller';
+use Mojo::UserAgent;
+
+use Data::Dumper;
 
 sub get {
     my $self = shift;
 
     my ( $day, $month ) = ( $self->stash('day'), $self->stash('month') );
+
+    # Fresh user agent
+    my $ua     = Mojo::UserAgent->new;
+    my $json   = $ua->get( $self->app->{config}->{'events'}->{'url'} );
+    my $events = $json->res->json;
+
+    #TODO: find artists from events
+    #TODO: generate playlist from the artists
 
     my $message = "Events";
     if ( defined $day && defined $month ) {
