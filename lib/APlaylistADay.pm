4c3d5fb8bc0a6adb291e8a7781e24cea5f1c9fd5
Carlos Rosão <shemahmforash@gmail.com>
2013-12-08 20:03:51 +0000

    Get configuration values according to enviromnent

--- lib/APlaylistADay.pm
+++ lib/APlaylistADay.pm
@@ -1,19 +1,28 @@
 package APlaylistADay;
 use Mojo::Base 'Mojolicious';
 
+use Data::Dumper;
+
 # This method will run once at server start
 sub startup {
     my $self = shift;
 
     #keep application config
-    my $config = $self->plugin(
+    my $cfg = $self->plugin(
         yaml_config => {
             file      => 'etc/config.yaml',
             stash_key => 'conf',
             class     => 'YAML::XS'
         }
     );
-    $self->{'config'} = $config;
+
+    #merging default environment config with current environment config
+    my %config = %{ $cfg->{'development'} || {} };
+    if ( $self->app->mode ne 'development' ) {
+        %config = ( %config, %{ $cfg->{ $self->app->mode } } );
+    }
+
+    $self->{'config'} = \%config;
 
     # Router
     my $r = $self->routes;
